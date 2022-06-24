c59bada788340cacc329649f7dcbe555eb254161
Lukas <L_Killgore@protonmail.com>
2022-06-24 07:42:35 -0700

    prod

--- db/index.js
+++ db/index.js
@@ -1,12 +1,3 @@
 const db = require('./models');
 
 module.exports = db.sequelize.models;
-
-
-// Using PG package instead of sequelize
-// const { Pool } = require("pg");
-
-// const pool = new Pool();
-// module.exports = {
-//     query: (text, params) => pool.query(text, params),
-// };
