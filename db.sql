a5443699539c87dc910118f491d2c62e653dda19
Lukas <L_Killgore@protonmail.com>
2022-07-12 23:05:24 -0700

    added source model

--- db.sql
+++ db.sql
@@ -8,53 +8,6 @@ CREATE TABLE accounts (
     updated_at TIMESTAMPTZ NOT NULL
 );
 
-CREATE TABLE predictions (
-    prediction_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES accounts(user_id) ON DELETE CASCADE,
-    user_prediction_status VARCHAR ( 7 ) NOT NULL,
-    claim_title VARCHAR ( 100 ) NOT NULL,
-    -- consider increasing claim_title limit to 264 as well
-    claim_major VARCHAR ( 264 ) NOT NULL,
-    post_time TIMESTAMPTZ NOT NULL,
-    timeframe TIMESTAMPTZ NOT NULL,
-    status VARCHAR ( 10 ) NOT NULL,
-    conc_reason TEXT,
-    conc_reason_timestamp TIMESTAMPTZ,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL
-);
-
-CREATE TABLE reasons (
-    reason_id SERIAL PRIMARY KEY,
-    prediction_id INT NOT NULL REFERENCES predictions(prediction_id) ON DELETE CASCADE,
-    reason TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL
-);
-
-CREATE TABLE votes (
-    vote_id SERIAL PRIMARY KEY,
-    prediction_id INT NOT NULL REFERENCES predictions(prediction_id) ON DELETE CASCADE,
-    user_id INT NOT NULL REFERENCES accounts(user_id) ON DELETE CASCADE,
-    plausible BOOLEAN,
-    correct BOOLEAN,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL
-);
-
-CREATE TABLE prediction_vote_tallies (
-    tally_id SERIAL PRIMARY KEY,
-    prediction_id INT NOT NULL REFERENCES predictions(prediction_id) ON DELETE CASCADE,
-    plausible INT DEFAULT 0,
-    implausible INT DEFAULT 0,
-    correct INT DEFAULT 0,
-    incorrect INT DEFAULT 0,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL
-);
-
-
--- HAVE TO ADD THESE COMMENT MODELS MANUALLY INTO APP DATABASE --
 CREATE TABLE comments (
     comment_id SERIAL PRIMARY KEY,
     prediction_id INT NOT NULL REFERENCES predictions(prediction_id) ON DELETE CASCADE,
@@ -81,9 +34,73 @@ CREATE TABLE comment_votes (
     updated_at TIMESTAMPTZ NOT NULL
 );
 
+CREATE TABLE predictions (
+    prediction_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES accounts(user_id) ON DELETE CASCADE,
+    user_prediction_status VARCHAR ( 7 ) NOT NULL,
+    claim_title VARCHAR ( 100 ) NOT NULL,
+    -- consider increasing claim_title limit to 264 as well
+    claim_major VARCHAR ( 264 ) NOT NULL,
+    post_time TIMESTAMPTZ NOT NULL,
+    timeframe TIMESTAMPTZ NOT NULL,
+    status VARCHAR ( 10 ) NOT NULL,
+    conc_reason TEXT,
+    conc_reason_timestamp TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE prediction_vote_tallies (
+    tally_id SERIAL PRIMARY KEY,
+    prediction_id INT NOT NULL REFERENCES predictions(prediction_id) ON DELETE CASCADE,
+    plausible INT DEFAULT 0,
+    implausible INT DEFAULT 0,
+    correct INT DEFAULT 0,
+    incorrect INT DEFAULT 0,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE reasons (
+    reason_id SERIAL PRIMARY KEY,
+    prediction_id INT NOT NULL REFERENCES predictions(prediction_id) ON DELETE CASCADE,
+    reason TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
 
 CREATE TABLE sources (
     source_id SERIAL PRIMARY KEY,
     reason_id INT NOT NULL REFERENCES reasons(reason_id) ON DELETE CASCADE,
-    source VARCHAR ( 264 ) NOT NULL
+    source_type VARCHAR ( 24 ),
+    author1_last VARCHAR ( 264 ),
+    author1_initial VARCHAR ( 10 ),
+    author1_first VARCHAR ( 264 ),
+    author2_last VARCHAR ( 264 ),
+    author2_initial VARCHAR ( 10 ),
+    author2_first VARCHAR ( 264 ),
+    et_al BOOLEAN DEFAULT FALSE,
+    publication_date DATE,
+    accessed_date DATE,
+    title VARCHAR ( 400 ),
+    edition VARCHAR ( 15 ),
+    volume VARCHAR ( 15 ),
+    issue VARCHAR ( 15 ),
+    pages VARCHAR ( 15 ),
+    database_name VARCHAR ( 264 ),
+    publisher_name VARCHAR ( 264 ),
+    uploader_name VARCHAR ( 264 ),
+    url TEXT,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE votes (
+    vote_id SERIAL PRIMARY KEY,
+    prediction_id INT NOT NULL REFERENCES predictions(prediction_id) ON DELETE CASCADE,
+    user_id INT NOT NULL REFERENCES accounts(user_id) ON DELETE CASCADE,
+    plausible BOOLEAN,
+    correct BOOLEAN,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
 );
